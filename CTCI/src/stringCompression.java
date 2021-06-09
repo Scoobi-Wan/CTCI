d32a0531934c37a2e98bfd57dd3b6a5c3ae01aa4
paulr <paulrichdev@gmail.com>
2021-06-09 16:36:54 -0700

    added initial comments

--- CTCI/src/stringCompression.java
+++ CTCI/src/stringCompression.java
@@ -1,6 +1,11 @@
 
 public class stringCompression {
-
+	
+	/*
+	 * Author: Paul Rich
+	 * Problem: Cracking The Coding Interview 1.6
+	 * Notes: 
+	 */
 	public String solution(String str) {
 		String compressed = "";
 		
