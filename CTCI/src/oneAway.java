7302bbef70829af67186e13c89251baae52de524
paulr <paulrichdev@gmail.com>
2021-06-09 14:58:32 -0700

    Solved

--- CTCI/src/oneAway.java
+++ CTCI/src/oneAway.java
@@ -1,15 +1,44 @@
+import java.util.HashMap;
 
 public class oneAway {
 
 	public boolean solution(String str1, String str2) {
 		
 		int i = 0;
+		int j = 0;
 		int differences = 0;
 		
-		while (i < str1.length() || i < str2.length()) {
-			
+		// in this case more than 1 letter would have to be changed
+		if (Math.abs(str1.length()- str2.length()) > 1) {
+			return false;
 		}
 		
-		return true;
+		while (i < str1.length() && j < str2.length() && differences <= 1) {
+			if (str1.charAt(i) != str2.charAt(j)) {
+				differences++;
+				if (str1.charAt(i + 1) == str2.charAt(j)) {
+					i++;
+					continue;
+				} else if (str1.charAt(i) == str2.charAt(j + 1)) {
+					j++;
+					continue;
+				} else if (str1.charAt(i + 1) == str2.charAt(j + 1)) {
+					i++;
+					j++;
+					continue;
+				} else {
+					return false;
+				}
+			}
+			
+			i++;
+			j++;
+		}
+		
+		if (i < str1.length() || j < str2.length()) {
+			differences++;
+		}
+		
+		return differences < 2 ? true : false;
 	}
 }
---
+++ CTCI/src/oneAwayTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class oneAwayTest {
+
+	@Test
+	void test() {
+		oneAway myOneAway = new oneAway();
+		assertTrue(myOneAway.solution("pale", "ple"));
+		assertTrue(myOneAway.solution("pales", "pale"));
+		assertTrue(myOneAway.solution("pale", "bale"));
+		assertTrue(myOneAway.solution("ability", "ablity"));
+		assertFalse(myOneAway.solution("pale", "bake"));
+		assertFalse(myOneAway.solution("ppale", "apple"));
+	}
+
+}
