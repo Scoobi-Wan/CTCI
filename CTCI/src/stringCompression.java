a85433db3df67c9409f37404140baf2f232f3154
paulr <paulrichdev@gmail.com>
2021-06-09 16:58:07 -0700

    initial solution

--- CTCI/src/stringCompression.java
+++ CTCI/src/stringCompression.java
@@ -9,8 +9,15 @@ public class stringCompression {
 	public String solution(String str) {
 		String compressed = "";
 		
+		for (int i = 0; i < str.length();) {
+			int inARow = 1;
+			while (i + inARow < str.length() && str.charAt(i + inARow) == str.charAt(i)) {
+				inARow++;
+			}
+			compressed += String.valueOf(str.charAt(i)) + inARow;
+			i += inARow;
+		}
 		
-		
-		return compressed;
+		return compressed.length() > str.length() ? compressed : str;
 	}
 }
