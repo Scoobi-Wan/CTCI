79035a0f302c7f94cca2320e4f5064295f767e06
paulr <paulrichdev@gmail.com>
2021-06-09 17:25:46 -0700

    initial commit

---
+++ CTCI/src/rotateMatrix.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class rotateMatrix {
+
+	public int[][] solution(int[][] matrix) {
+		
+		ArrayList<Integer> row = new ArrayList<Integer>();
+		
+		int j = 0;
+		
+		for (int i = matrix[0].length; i >= 0; i--) {
+			int temp = matrix[0][i];
+			matrix[0][i] = matrix[matrix.length][j];
+			matrix[matrix.length][j] = temp;
+			j++;
+		}
+		
+		return matrix;
+	}
+}
