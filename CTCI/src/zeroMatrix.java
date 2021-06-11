9265ea1e8f87ab0f5f164cb1078f2e50e3c908db
paulr <paulrichdev@gmail.com>
2021-06-11 15:32:20 -0700

    wip

--- CTCI/src/zeroMatrix.java
+++ CTCI/src/zeroMatrix.java
@@ -1,4 +1,34 @@
 
 public class zeroMatrix {
-	
+	public int[][] solution(int[][] input) {
+		
+		int[][] toReturn = new int[input.length][input[0].length];
+		
+		for (int i = 0; i < input.length; i++) {
+			for (int j = 0; j < input[i].length; j++) {
+				if (input[i][j] == 0) {
+					int first = 0;
+					int last = input.length;
+					while (first < last) {
+						toReturn[i][first] = 0;
+						first++;
+					}
+					first = 0;
+					last = input[0].length;
+					while (first < last) {
+						toReturn[first][j] = 0;
+						first++;
+					}
+				} else {
+					if (toReturn[i][j] != 0 ) {
+						toReturn[i][j] = input[i][j];
+					}
+				}
+			}
+		}
+		
+		
+		
+		return toReturn;
+	}
 }
---
+++ CTCI/src/zeroMatrixTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class zeroMatrixTest {
+
+	@Test
+	void test() {
+		zeroMatrix myZ = new zeroMatrix();
+		int[][] input = {{1,2,3,4,5},{6,7,0,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,0}};
+		int[][] output = myZ.solution(input);
+		assertEquals(output[2][2], 0);
+		assertEquals(output[1][2], 0);
+		assertEquals(output[1][1], 0);
+		assertEquals(output[1][0], 0);
+		assertEquals(output[3][3], 19);
+		assertEquals(output[1][0], 0);
+		assertEquals(output[1][0], 0);
+		
+	}
+				
+}
