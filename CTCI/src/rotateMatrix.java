c497c291defbbc181f7b5bf6605d4972a1472a45
paulr <paulrichdev@gmail.com>
2021-06-11 14:21:33 -0700

    swappa dappa doo

--- CTCI/src/rotateMatrix.java
+++ CTCI/src/rotateMatrix.java
@@ -8,10 +8,16 @@ public class rotateMatrix {
 		
 		int k = matrix.length;
 		
-		for (int i = matrix.length; i >= 0; i--) {
+		for (int i = 0; i < matrix.length / 2; i++) {
 			for (int j = 0; j < matrix.length; j++) {
+				int l = i - j;
 				int temp = matrix[j][k];
 				matrix[j][k] = matrix[i][j];
+				matrix[i][j] = matrix[k-l][i];
+				matrix[k-l][i] = matrix[k][k-l];
+				matrix[k][k-l] = matrix[i][k];
+				matrix[i][k] = temp;
+				
 				
 			}
 			
