7377aa84a7ab1da4815c754fa6e1da198c13813b
paulr <paulrichdev@gmail.com>
2021-06-13 14:10:23 -0700

    wip

--- CTCI/src/chapter2/sumLists.java
+++ CTCI/src/chapter2/sumLists.java
@@ -3,7 +3,7 @@ package chapter2;
 public class sumLists {
 
 	public Node solution(Node node1, Node node2) {
-		int sum = node1.val = node2.val;
+		int sum = node1.val + node2.val;
 		int digitSum = sum % 10;
 		int carryDigit = sum / 10;
 		Node head = new Node(digitSum);
@@ -11,24 +11,29 @@ public class sumLists {
 		node1 = node1.next;
 		node2 = node2.next;
 		while (node1 != null && node2 != null) {
-			digitSum = sum % 10;
-			sumNode.next = new Node(digitSum + carryDigit);
+			sum = node1.val + node2.val;
+			digitSum = (sum + carryDigit) % 10;
+			sumNode.next = new Node(digitSum);
 			sumNode = sumNode.next;
 			carryDigit = sum / 10;
 			node1 = node1.next;
 			node2 = node2.next;
 		}
 		
-		if (node1 != null && node2 == null) {
-			digitSum = node1.val + carryDigit;
-			sumNode.next = new Node(digitSum + carryDigit);
+		while (node1 != null && node2 == null) {
+			sum = node1.val + carryDigit;
+			digitSum = sum % 10;
+			carryDigit = sum / 10;
+			sumNode.next = new Node(digitSum);
 			sumNode = sumNode.next;
 			node1 = node1.next;
 		}
 		
-		if (node1 == null && node2 != null) {
-			digitSum = node2.val = carryDigit;
-			sumNode.next = new Node(digitSum = carryDigit);
+		while (node1 == null && node2 != null) {
+			sum = node2.val + carryDigit;
+			digitSum = sum % 10;
+			carryDigit = sum / 10;
+			sumNode.next = new Node(digitSum);
 			sumNode = sumNode.next;
 			node2 = node2.next;
 		}
--- CTCI/src/chapter2/sumListsTest.java
+++ CTCI/src/chapter2/sumListsTest.java
@@ -9,7 +9,18 @@ class sumListsTest {
 	@Test
 	void test() {
 		sumLists sl = new sumLists();
+		Node num1 = new Node(7);
+		num1.next = new Node(1);
+		num1.next = new Node(6);
 		
+		Node num2 = new Node(5);
+		num2.next = new Node(9);
+		num2.next.next = new Node(2);
+		
+		Node sum = sl.solution(num1, num2);
+		assertEquals(sum.val, 2);
+		assertEquals(sum.next.val, 1);
+		assertEquals(sum.next.next.val, 9);
 	}
 
 }
