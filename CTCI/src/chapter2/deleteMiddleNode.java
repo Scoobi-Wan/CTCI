438bc7280dad611aa588f87d6cd689f0cd00ec52
paulr <paulrichdev@gmail.com>
2021-06-13 12:59:40 -0700

    init comments

--- CTCI/src/chapter2/deleteMiddleNode.java
+++ CTCI/src/chapter2/deleteMiddleNode.java
@@ -2,6 +2,13 @@ package chapter2;
 
 public class deleteMiddleNode {
 
+	/*
+	 * Author: Paul Rich
+	 * Problem: Cracking The Coding Interview 2.3
+	 * Notes: Does not mean delete the exact middle, but rather deleting a node not at the start or end of a linked list.
+	 * 		  Only given access to the node to delete
+	 *
+	 */
 	public void solution(Node node) {
 		node.val = node.next.val;
 		node.next = node.next.next;
---
+++ CTCI/src/chapter2/deleteMiddleNodeTest.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class deleteMiddleNodeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
