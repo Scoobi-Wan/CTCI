ada22f69c54c70679e95958723f2ad64d4a74a38
paulr <paulrichdev@gmail.com>
2021-06-13 12:37:34 -0700

    pre test

--- CTCI/src/chapter2/returnKthToLast.java
+++ CTCI/src/chapter2/returnKthToLast.java
@@ -2,4 +2,31 @@ package chapter2;
 
 public class returnKthToLast {
 
+	public Node solution(Node node, int toLastIndex) {
+		
+		Node head = node;
+		
+		int len = 0;
+		
+		while (node != null) {
+			len++;
+			node = node.next;
+		}
+		
+		node = head;
+		
+		int targetIndex = len - toLastIndex;
+		int i = 0;
+		
+		while (i < targetIndex) {
+			node = node.next;
+			i++;
+		}
+		
+		
+		
+		return node;
+		
+	}
+	
 }
