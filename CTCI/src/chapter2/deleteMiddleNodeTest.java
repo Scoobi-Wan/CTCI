c17fb3fdcf9c659d3c466007dc22ededa2c885a9
paulr <paulrichdev@gmail.com>
2021-06-13 13:06:42 -0700

    solved + test

--- CTCI/src/chapter2/deleteMiddleNodeTest.java
+++ CTCI/src/chapter2/deleteMiddleNodeTest.java
@@ -8,7 +8,19 @@ class deleteMiddleNodeTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Node node = new Node(5);
+		node.next = new Node(10);
+		node.next.next = new Node(15);
+		node.next.next.next = new Node(20);
+		node.next.next.next.next = new Node(25);
+		node.next.next.next.next.next = new Node(30);
+		deleteMiddleNode dmn = new deleteMiddleNode();
+		Node toDelete = node.next.next.next;
+		dmn.solution(toDelete);
+		assertEquals(node.next.next.next.val, 25);
+		assertEquals(node.next.next.next.next.val, 30);
+		dmn.solution(toDelete);
+		assertEquals(node.next.next.next.val, 30);
 	}
 
 }
