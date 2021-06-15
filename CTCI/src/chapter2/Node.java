1def7948989ab8fa36b6f65c1824651aa7090e73
paulr <paulrichdev@gmail.com>
2021-06-15 13:53:57 -0700

    uodated Node class with String field

--- CTCI/src/chapter2/Node.java
+++ CTCI/src/chapter2/Node.java
@@ -4,10 +4,15 @@ package chapter2;
 public class Node {
 		
 	int val;
+	String str;
 	Node next;
 	
 	public Node(int val) {
 		this.val = val;
+	}
+	
+	public Node(String str) {
+		this.str = str;
 	}
 
 }
--- CTCI/src/chapter2/palindrome.java
+++ CTCI/src/chapter2/palindrome.java
@@ -1,8 +1,15 @@
 package chapter2;
 
+import java.util.ArrayList;
+
 public class palindrome {
 
 	public Node solution(Node node) {
+		
+		ArrayList<Character> myStack = new ArrayList<Character>();
+		
+		
+		
 		Node head = node;
 		
 		
